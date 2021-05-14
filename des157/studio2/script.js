d640f59ce3bc23fc9e7b100923893ed7f7f1e605
David Manh Hoang <davemhoang@gmail.com>
2021-05-14 03:33:40 -0700

    added books in position

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -1,4 +1,16 @@
 (function(){
 
-    
+    const book1 = document.getElementById("book1");
+    const book2 = document.getElementById("book2");
+    const book3 = document.getElementById("book3");
+    const book4 = document.getElementById("book4");
+    const book5 = document.getElementById("book5");
+    const book6 = document.getElementById("book6");
+    const book7 = document.getElementById("book7");
+    const book8 = document.getElementById("book8");
+
+    book1.addEventListener('mouseover', function() {
+        console.log("book1 was pressed");
+    });
+
 })
