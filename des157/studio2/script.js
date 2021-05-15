2137a7fbb1465f52b0f5b5f7ca38c39ef21a854a
DavidMHoang99 <56091705+DavidMHoang@users.noreply.github.com>
2021-05-14 19:14:39 -0700

    added some more images to books

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -12,26 +12,26 @@
     const book8 = document.getElementById("book8");
 
     
-    book1.addEventListener('click', function() {
-        const modal = document.getElementById("book1Modal");
+    // book1.addEventListener('click', function() {
+    //     const modal = document.getElementById("book1Modal");
         
-        modal.style.display = "block";
-        window.onclick = function(event) {
-            if(event.target==modal) {
-                modal.style.display = "none";
-            }
-        }
-    });
-    book2.addEventListener('click', function() {
-        const modal = document.getElementById("book2Modal");
+    //     modal.style.display = "block";
+    //     window.onclick = function(event) {
+    //         if(event.target==modal) {
+    //             modal.style.display = "none";
+    //         }
+    //     }
+    // });
+    // book2.addEventListener('click', function() {
+    //     const modal = document.getElementById("book2Modal");
         
-        modal.style.display = "block";
-        window.onclick = function(event) {
-            if(event.target==modal) {
-                modal.style.display = "none";
-            }
-        }
-    });
+    //     modal.style.display = "block";
+    //     window.onclick = function(event) {
+    //         if(event.target==modal) {
+    //             modal.style.display = "none";
+    //         }
+    //     }
+    // });
     book3.addEventListener('click', function() {
         const modal = document.getElementById("book3Modal");
         
@@ -41,56 +41,93 @@
                 modal.style.display = "none";
             }
         }
-    });
-    book4.addEventListener('click', function() {
-        const modal = document.getElementById("book4Modal");
+
+        function zeroPad(num, places) {
+            var zero = places - num.toString().length + 1;
+            return Array(+(zero > 0 && zero)).join("0") + num;
+        }
         
-        modal.style.display = "block";
-        window.onclick = function(event) {
-            if(event.target==modal) {
-                modal.style.display = "none";
+        document.addEventListener('mousemove', reportPos);
+        const theImg = document.getElementById("sunset");
+        let prevLoc = 0;
+
+        function reportPos(event) {
+        const windowSize = window.innerWidth;
+        const percent2px = windowSize / 1400;
+        const xPos = event.clientX;
+        const changePhoto = zeroPad(Math.floor(xPos / percent2px)+1, 5);
+        zeroPad(changePhoto, 5);
+        if (changePhoto !== prevLoc) {
+                theImg.src = `minecraft/scene${changePhoto}.jpg`;
+                prevLoc = changePhoto;
+                console.log(prevLoc);
             }
         }
+
     });
-    book5.addEventListener('click', function() {
-        const modal = document.getElementById("book5Modal");
+    // book4.addEventListener('click', function() {
+    //     const modal = document.getElementById("book4Modal");
         
-        modal.style.display = "block";
-        window.onclick = function(event) {
-            if(event.target==modal) {
-                modal.style.display = "none";
-            }
-        }
-    });
-    book6.addEventListener('click', function() {
-        const modal = document.getElementById("book6Modal");
+    //     modal.style.display = "block";
+    //     window.onclick = function(event) {
+    //         if(event.target==modal) {
+    //             modal.style.display = "none";
+    //         }
+    //     }
+    // });
+    // book5.addEventListener('click', function() {
+    //     const modal = document.getElementById("book5Modal");
         
-        modal.style.display = "block";
-        window.onclick = function(event) {
-            if(event.target==modal) {
-                modal.style.display = "none";
-            }
-        }
-    });
-    book7.addEventListener('click', function() {
-        const modal = document.getElementById("book7Modal");
+    //     modal.style.display = "block";
+    //     window.onclick = function(event) {
+    //         if(event.target==modal) {
+    //             modal.style.display = "none";
+    //         }
+    //     }
+    // });
+    // book6.addEventListener('click', function() {
+    //     const modal = document.getElementById("book6Modal");
         
-        modal.style.display = "block";
-        window.onclick = function(event) {
-            if(event.target==modal) {
-                modal.style.display = "none";
-            }
-        }
-    });
-    book8.addEventListener('click', function() {
-        const modal = document.getElementById("book8Modal");
+    //     modal.style.display = "block";
+    //     window.onclick = function(event) {
+    //         if(event.target==modal) {
+    //             modal.style.display = "none";
+    //         }
+    //     }
+    // });
+    // book7.addEventListener('click', function() {
+    //     const modal = document.getElementById("book7Modal");
         
-        modal.style.display = "block";
-        window.onclick = function(event) {
-            if(event.target==modal) {
-                modal.style.display = "none";
+    //     modal.style.display = "block";
+    //     window.onclick = function(event) {
+    //         if(event.target==modal) {
+    //             modal.style.display = "none";
+    //         }
+    //     }
+    // });
+    // book8.addEventListener('click', function() {
+    //     const modal = document.getElementById("book8Modal");
+        
+    //     modal.style.display = "block";
+    //     window.onclick = function(event) {
+    //         if(event.target==modal) {
+    //             modal.style.display = "none";
+    //         }
+    //     }
+    // });
+
+    for (let i=1; i<9; i++) {
+        document.getElementById(`book${i}`).addEventListener('click', function() {
+            var modal = document.getElementById(`book${i}Modal`);
+            
+            modal.className="modal hidden";
+            modal.style.display = "block";
+            window.onclick = function(event) {
+                if(event.target==modal) {
+                    modal.style.display = "none";
+                }
             }
-        }
-    });
+        })
+    }
 
 })();
