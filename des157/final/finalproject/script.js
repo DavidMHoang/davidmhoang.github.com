a4b58f442e0ad1118ec0eb214e52bbf8c055e2e1
DavidMHoang99 <56091705+DavidMHoang@users.noreply.github.com>
2021-06-07 13:22:32 -0700

    working on final

---
+++ des157/final/finalproject/script.js
@@ -0,0 +1,61 @@
+(function(){
+
+    "use strict";
+
+    const windSound = new Audio('./media/wind.wav');
+
+    alert("Hello \n Please test the scrolling to see if it will follow. Does it scroll and follow? Is this a good idea? \n Please test tapping on an icon. Do you think this is helpful? Do you think it can be improved? \n Please test the sound if it is working. Is the sound needed or make a difference?");
+    const item1 = document.getElementById("item1");
+    const item2 = document.getElementById("item2");
+    const item3 = document.getElementById("item3");
+    const item4 = document.getElementById("item4");
+    const soundHandler = document.getElementById("soundControl");
+
+    item3.addEventListener('click', function() {
+        const modal = document.getElementById("item3Modal");
+        
+        modal.style.display = "block";
+        window.onclick = function(event) {
+            if(event.target==modal) {
+                modal.style.display = "none";
+            }
+        }
+
+        function zeroPad(num, places) {
+            var zero = places - num.toString().length + 1;
+            return Array(+(zero > 0 && zero)).join("0") + num;
+        }
+        
+        document.addEventListener('mousemove', reportPos);
+        const theImg = document.getElementById("sunset");
+        let prevLoc = 0;
+
+        function reportPos(event) {
+        const windowSize = window.innerWidth;
+        const percent2px = windowSize / 1400;
+        const xPos = event.clientX;
+        const changePhoto = zeroPad(Math.floor(xPos / percent2px)+1, 5);
+        zeroPad(changePhoto, 5);
+        if (changePhoto !== prevLoc) {
+                theImg.src = `minecraft/scene${changePhoto}.jpg`;
+                prevLoc = changePhoto;
+                console.log(prevLoc);
+            }
+        }
+
+    });
+
+    for (let i=1; i<9; i++) {
+        document.getElementById(`item${i}`).addEventListener('click', function() {
+            var modal = document.getElementById(`item${i}Modal`);
+            
+            modal.style.display = "block";
+            window.onclick = function(event) {
+                if(event.target==modal) {
+                    modal.style.display = "none";
+                }
+            }
+        })
+    }
+
+})();
