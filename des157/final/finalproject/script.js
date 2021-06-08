c1cf1be9660005e484524e856b7f3b61f547a23b
David Manh Hoang <davemhoang@gmail.com>
2021-06-08 01:19:39 -0700

    working on final

--- des157/final/finalproject/script.js
+++ des157/final/finalproject/script.js
@@ -10,40 +10,6 @@
     const item4 = document.getElementById("item4");
     const soundHandler = document.getElementById("soundControl");
 
-    item3.addEventListener('click', function() {
-        const modal = document.getElementById("item3Modal");
-        
-        modal.style.display = "block";
-        window.onclick = function(event) {
-            if(event.target==modal) {
-                modal.style.display = "none";
-            }
-        }
-
-        function zeroPad(num, places) {
-            var zero = places - num.toString().length + 1;
-            return Array(+(zero > 0 && zero)).join("0") + num;
-        }
-        
-        document.addEventListener('mousemove', reportPos);
-        const theImg = document.getElementById("sunset");
-        let prevLoc = 0;
-
-        function reportPos(event) {
-        const windowSize = window.innerWidth;
-        const percent2px = windowSize / 1400;
-        const xPos = event.clientX;
-        const changePhoto = zeroPad(Math.floor(xPos / percent2px)+1, 5);
-        zeroPad(changePhoto, 5);
-        if (changePhoto !== prevLoc) {
-                theImg.src = `minecraft/scene${changePhoto}.jpg`;
-                prevLoc = changePhoto;
-                console.log(prevLoc);
-            }
-        }
-
-    });
-
     for (let i=1; i<9; i++) {
         document.getElementById(`item${i}`).addEventListener('click', function() {
             var modal = document.getElementById(`item${i}Modal`);
